c6877e25c71f7f3caa250c00e09eeeaa5a8309ce
Ali0lo <aliaze975@gmail.com>
2025-12-08 06:12:29 +0000

    salam

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python3
+
 class Square:
+
     def __init__(self, size):
-        self.__size = sizeâ
+        self.__size = size
+
+if __name__ == "__main__":
+    mysquare = Square(3)
+    print(type(mysquare))
+    print(mysquare.__dict__)
+
+    mysquare = Square(89)
+    print(type(mysquare))
+    print(mysquare.__dict__)
+
+    try:
+        print(mysquare.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(mysquare.__size)
+    except Exception as e:
+        print(e)
