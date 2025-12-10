405a4ad3c54d7fd1dab36ca6b2898a40e5a4e936
Ali0lo <aliaze975@gmail.com>
2025-12-10 19:46:55 +0000

    hello

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Define a Square class with a private size attribute."""
+
+
 class Square:
+    """Square class with a private __size attribute."""
     def __init__(self, size):
         self.__size = size
+
+
 
 
